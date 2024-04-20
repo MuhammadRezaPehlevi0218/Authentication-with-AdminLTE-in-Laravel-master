84fc746a79534554b149ea287d3079edec9fe8fe
MuhammadRezaPehlevi0218 <78774220+MuhammadRezaPehlevi0218@users.noreply.github.com>
2024-04-20 14:55:44 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+User/Admin authentication with AdminLTE dashboard
+Used technologies: Laravel, MySQL
+
+![User authentication](user_login.png)
+![User dashboard](user_startpage.png)
+![Admin authentication](admin_login.png)
+![Admin dashboard](admin_startpage.png)
---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Constanta, Romania
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2024 </strong>
+  </footer>
